428ca6cfa74610e07ea4c111523247d8d09ee353
Przemek <pzadomski@gmail.com>
2019-02-01 16:59:53 +0100

    Falling bricks

--- gra/tetris.js
+++ gra/tetris.js
@@ -13,21 +13,4 @@ const board = document.getElementById('board');
             ]
         }
 
-        const cells = Array.from(board.querySelectorAll('.cell'));
-        const x = 0;
-        const y = 0;
-        const boardSize = 32;
-        const cellsInFirstRow = cells.filter((cell, index) => index >= x + boardSize * y && index < x + 4 + boardSize * y)
-        const cellsInSecondRow = cells.filter((cell, index) => index >= x + (boardSize * (y + 1)) && index < x + 4 + (boardSize * (y + 1)))
-        const cellsInThirdRow = cells.filter((cell, index) => index >= x + (boardSize * (y + 2)) && index < x + 4 + (boardSize * (y + 2)))
-        const cellsInFourthRow = cells.filter((cell, index) => index >= x + (boardSize * (y + 3)) && index < x + 4 + (boardSize * (y + 3)))
-
-        const ourCells = cellsInFirstRow.concat(cellsInSecondRow, cellsInThirdRow, cellsInFourthRow);
-
-        const ourL = blocks.L[0].join('').split('')
-
-        // for (let i = 0; i < 16; i += 1) {
-        //     if (ourL[i] === '#') {
-        //         ourCells[i].classList.add('fruit')
-        //     }
-        // }
+        
