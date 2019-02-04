16a4ece57fb90ed1ac8c6e0643687b78c6671cb0
overkoper <piotr.kopczynski7@gmail.com>
2019-02-04 17:03:54 +0100

    take out functions and added keyboard controls (left arrow, right arrow, down arrow)

--- gra/bricks.js
+++ gra/bricks.js
@@ -1,47 +1,58 @@
 let bricks = document.querySelectorAll('.fruit');
 
-
 const cells = Array.from(board.querySelectorAll('.cell'));
 
 let y = 0;
-var x = 0;
+let x = 0;
 
-setInterval(function () {
-  console.log(window.x);
-  y += 1;
+addEventListener("keydown", function (event) {
+  if (event.code === "ArrowRight") {
+    x++;
+    paintingBricks()
+  }
+  if (event.code === "ArrowLeft") {
+    x--;
+    paintingBricks()
+  }
+  if (event.code === "ArrowDown") {
+    y++;
+    paintingBricks();
+  }
+});
+
+function getCellsWeWantToPaint() {
   const boardSize = 32;
   const cellsInFirstRow = cells.filter((cell, index) => index >= x + boardSize * y && index < x + 4 + boardSize * y)
   const cellsInSecondRow = cells.filter((cell, index) => index >= x + (boardSize * (y + 1)) && index < x + 4 + (boardSize * (y + 1)))
   const cellsInThirdRow = cells.filter((cell, index) => index >= x + (boardSize * (y + 2)) && index < x + 4 + (boardSize * (y + 2)))
   const cellsInFourthRow = cells.filter((cell, index) => index >= x + (boardSize * (y + 3)) && index < x + 4 + (boardSize * (y + 3)))
 
-
-
   const ourCells = cellsInFirstRow.concat(cellsInSecondRow, cellsInThirdRow, cellsInFourthRow);
 
   const ourL = blocks.BigO[3].join('').split('')
 
+  const hashIndexes = ourL.map((symbol, index) => ({ symbol, index })).filter(item => item.symbol === '#').map(item => item.index);
 
+  return hashIndexes.map(index => ourCells[index])
+}
 
-  const hashIndices = ourL.map((symbol, index) => ({ symbol, index })).filter(item => item.symbol === '#').map(item => item.index);
-  // console.log(hashIndices);
-
-  const cellsWeWantToPaint = hashIndices.map(index => ourCells[index])
+function paintingBricks() {
+  const cellsWeWantToPaint = getCellsWeWantToPaint();
   const cellsWeHavePainted = document.querySelectorAll('.fruit:not(.blocked)');
 
-  const weCanGo = cellsWeWantToPaint.every(cell => {
-    // console.log(cell);
-    // console.log(cell.classList.toString())
-    return cell !== undefined && !cell.classList.contains('blocked')
+  // wyciągnąć funkcję na zewnątrz
+  const weCanGoDown = cellsWeWantToPaint.every(cell => {
+    return cell !== undefined //&& !cell.classList.contains('blocked')
   })
-
-
-
-  if (weCanGo === false) {
-    // console.log('STOP')
-    // debugger;
-    y = 0;
+  if (weCanGoDown === false) {
+    y = -1;
     cellsWeHavePainted.forEach(item => item.classList.add('blocked'));
+    return;
+  }
+  const weCanGoSide = cellsWeWantToPaint.every(cell => {
+    return !cell.classList.contains("blocked");
+  }) 
+  if(weCanGoSide === false){
     return;
   }
 
@@ -49,5 +60,10 @@ setInterval(function () {
   cellsWeHavePainted.forEach(item => item.classList.remove('fruit'));
 
   cellsWeWantToPaint.forEach(cell => cell.classList.add('fruit'))
+}
+// spadanie klocków
 
+setInterval(function () {
+  y++;
+  paintingBricks();
 }, 200);
--- gra/collisions.js
+++ gra/collisions.js
@@ -1,35 +1,3 @@
 // przyjąłem prowizorycznny ruch klocków żeby ustalić kolizje. dla demonstracji tworzę klocka na 1 komórce i przeszkodę na 5 komórce. prowizoryczny ruch odbywa się tylko w prawo i lewo (sterowanie strzałkami). warunek kolizji jest ustawiony na krawędź planszy (null) i na elementy z klasą "blocked". 
 
 // przykładowy klocek
-let brickNode = document.querySelector(".cell");
-brickNode.classList.add("brick");
-// przykładowa przeszkoda
-let brickObstacle = document.querySelector(".cell:nth-child(5)");
-brickObstacle.classList.add("blocked");
-
-addEventListener("keyup", function(event){
-  if (event.code === "ArrowRight") {
-    let targetNode = brickNode.nextElementSibling;
-    if (targetNode === null || targetNode.classList.contains("blocked")) {
-      return;
-    }
-    // prowizoryczny ruch
-    moveBrick(brickNode, targetNode);
-    brickNode = document.querySelector(".brick")
-  }
-  if (event.code === "ArrowLeft") {
-    let targetNode = brickNode.previousElementSibling;
-    if (targetNode === null || targetNode.classList.contains("blocked")) {
-      return;
-    }
-    // prowizoryczny ruch
-    moveBrick(brickNode, targetNode);
-    brickNode = document.querySelector(".brick")
-    
-  }
-});
-  // prowizoryczny ruch
-  function moveBrick(fromNode, toNode) {
-    fromNode.classList.remove("brick");
-    toNode.classList.add("brick");
-  }
