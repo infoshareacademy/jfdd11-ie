52d56fb8445a225531103c87eaaf0d03aee529e1
Przemek <pzadomski@gmail.com>
2019-02-05 16:28:38 +0100

    With walls and new features

--- gra/bricks.js
+++ gra/bricks.js
@@ -1,47 +1,92 @@
-let bricks = document.querySelectorAll('.fruit');
-
-
+const bricks = document.querySelectorAll('.fruit');
+const boardSize = 32;
 const cells = Array.from(board.querySelectorAll('.cell'));
 
 let y = 0;
-var x = 0;
+let x = randomBrickStart();
 
-setInterval(function () {
-  y += 1;
-  const boardSize = 32;
+window.addEventListener("keydown", function (event) {
+  if (event.code === "ArrowRight") {
+    x++;
+    if (!weCanGo()) {
+      x--;
+    }
+  }
+  if (event.code === "ArrowLeft") {
+    x--;
+    if (!weCanGo()) {
+      x++;
+    }
+  }
+  if (event.code === "ArrowDown") {
+    y++;
+    if (!weCanGo()) {
+      y--;
+    }
+  }
+  paintingBricks();
+});
+let currentBrickName = randomBrick();
+let currentBrickFrame = 0;
+addEventListener ("keydown",function(event){
+  if(event.code === "Space"){
+    currentBrickFrame = (currentBrickFrame+1)%4;
+  }
+  });
+
+function randomBrickStart(){
+  return Math.floor(Math.random() * 28);
+}
+function randomBrick(){
+  return Object.keys(blocks)[Math.floor(Math.random() * Object.keys(blocks).length)];
+}
+
+function weCanGo() {
+  return getCellsWeWantToPaint().every(cell => {
+    return cell !== undefined && !cell.classList.contains('blocked')
+  })
+}
+function getCellsWeWantToPaint() {
+  
   const cellsInFirstRow = cells.filter((cell, index) => index >= x + boardSize * y && index < x + 4 + boardSize * y)
   const cellsInSecondRow = cells.filter((cell, index) => index >= x + (boardSize * (y + 1)) && index < x + 4 + (boardSize * (y + 1)))
   const cellsInThirdRow = cells.filter((cell, index) => index >= x + (boardSize * (y + 2)) && index < x + 4 + (boardSize * (y + 2)))
   const cellsInFourthRow = cells.filter((cell, index) => index >= x + (boardSize * (y + 3)) && index < x + 4 + (boardSize * (y + 3)))
 
-
-
   const ourCells = cellsInFirstRow.concat(cellsInSecondRow, cellsInThirdRow, cellsInFourthRow);
 
-  const ourL = blocks.L[2].join('').split('')
+  let newBrick = blocks[currentBrickName][currentBrickFrame].join('').split('')
 
+  const hashIndexes = newBrick.map((symbol, index) => ({ symbol, index })).filter(item => item.symbol === '#').map(item => item.index);
 
+  return hashIndexes.map(index => ourCells[index])
+}
 
-  const hashIndices = ourL.map((symbol, index) => ({ symbol, index })).filter(item => item.symbol === '#').map(item => item.index);
-
-  const cellsWeWantToPaint = hashIndices.map(index => ourCells[index])
+function paintingBricks() {
+  const cellsWeWantToPaint = getCellsWeWantToPaint();
   const cellsWeHavePainted = document.querySelectorAll('.fruit:not(.blocked)');
 
-  const weCanGo = cellsWeWantToPaint.every(cell => {
-    return cell !== undefined && !cell.classList.contains('blocked')
-  })
+  // wyciągnąć funkcję na zewnątrz
+  
+  if (!weCanGo()) {
 
 
-
-  if (weCanGo === false) {
-    y = 0;
     cellsWeHavePainted.forEach(item => item.classList.add('blocked'));
+    currentBrickName = randomBrick();
+    y = -1;
+    x = randomBrickStart()
     return;
   }
 
   // remove all existing cells
   cellsWeHavePainted.forEach(item => item.classList.remove('fruit'));
 
+  // paint new cells
   cellsWeWantToPaint.forEach(cell => cell.classList.add('fruit'))
+}
 
+// spadanie klocków
+setInterval(function () {
+  y++;
+  paintingBricks();
 }, 200);
