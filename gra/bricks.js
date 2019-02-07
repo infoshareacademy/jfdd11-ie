de356bb5166f6eeb5ba7cc8d086c17b2fdf1ff0e
Przemek <pzadomski@gmail.com>
2019-02-07 16:52:56 +0100

    Trucks with containers

--- gra/bricks.js
+++ gra/bricks.js
@@ -5,18 +5,18 @@ makeBoard(board, boardSize, 40);
 const bricks = document.querySelectorAll('.fruit');
 
 const cells = Array.from(board.querySelectorAll('.cell'));
+const allCells = cells.slice(0);
 let gameIsPaused = false;
 let y = 0;
 let x = randomBrickStart();
-addEventListener("keyup", function (event){
-if(event.code === "Escape"){
-  gameIsPaused = !gameIsPaused
-  console.log(gameIsPaused)
-}
+addEventListener("keyup", function (event) {
+  if (event.code === "Escape") {
+    gameIsPaused = !gameIsPaused
+  }
 
 })
 window.addEventListener("keydown", function (event) {
-  if(gameIsPaused){
+  if (gameIsPaused) {
     return;
   }
   if (event.code === "ArrowRight") {
@@ -37,20 +37,59 @@ window.addEventListener("keydown", function (event) {
       y--;
     }
   }
+  if (event.code === "KeyA") {
+    if (blockedTruckIds[1] === true) {
+      console.log("nie pośmigasz")
+    }
+    else {
+      truckNodes.forEach((el, index) => {
+        if (index % boardSize < 11 && index % boardSize > 0) {
+          el.classList.remove('fruit');
+          el.classList.remove('blocked');
+        }
+      })
+    }
+  }
+  if (event.code === "KeyS") {
+    if (blockedTruckIds[2] === true) {
+      console.log("nie pośmigasz")
+    }
+    else {
+      truckNodes.forEach((el, index) => {
+        if (index % boardSize < 22 && index % boardSize > 11) {
+          el.classList.remove('fruit');
+          el.classList.remove('blocked');
+        }
+      })
+    }
+  }
+  if (event.code === "KeyD") {
+    if (blockedTruckIds[3] === true) {
+      console.log("nie pośmigasz")
+    }
+    else {
+      truckNodes.forEach((el, index) => {
+        if (index % boardSize > 22 && index % boardSize < 33) {
+          el.classList.remove('fruit');
+          el.classList.remove('blocked');
+        }
+      })
+    }
+  }
   paintingBricks();
 });
 let currentBrickName = randomBrick();
 let currentBrickFrame = 0;
-addEventListener ("keyup",function(event){
-  if(event.code === "Space"){
-    currentBrickFrame = (currentBrickFrame+1)%4;
+addEventListener("keyup", function (event) {
+  if (event.code === "Space") {
+    currentBrickFrame = (currentBrickFrame + 1) % 4;
   }
-  });
+});
 
-function randomBrickStart(){
+function randomBrickStart() {
   return Math.floor(Math.random() * (boardSize - 4));
 }
-function randomBrick(){
+function randomBrick() {
   return Object.keys(blocks)[Math.floor(Math.random() * Object.keys(blocks).length)];
 }
 
@@ -60,7 +99,7 @@ function weCanGo() {
   })
 }
 function getCellsWeWantToPaint() {
-  
+
   const cellsInFirstRow = cells.filter((cell, index) => index >= x + boardSize * y && index < x + 4 + boardSize * y)
   const cellsInSecondRow = cells.filter((cell, index) => index >= x + (boardSize * (y + 1)) && index < x + 4 + (boardSize * (y + 1)))
   const cellsInThirdRow = cells.filter((cell, index) => index >= x + (boardSize * (y + 2)) && index < x + 4 + (boardSize * (y + 2)))
@@ -74,28 +113,64 @@ function getCellsWeWantToPaint() {
   return hashIndexes.map(index => ourCells[index])
 }
 
+function makeNewBrick() {
+  currentBrickName = randomBrick();
+  y = -1;
+  x = randomBrickStart();
+}
+
+const blockedTruckIds = {
+  1: false,
+  2: false,
+  3: false
+}
+
 function paintingBricks() {
   const cellsWeWantToPaint = getCellsWeWantToPaint();
   const cellsWeHavePainted = document.querySelectorAll('.fruit:not(.blocked)');
-  
+
   if (!weCanGo()) {
-    cellsWeHavePainted.forEach(item => item.classList.add('blocked'));
 
     // If not every cell we have painted has class truck
     if (Array.from(cellsWeHavePainted).some(cell => !cell.classList.contains('truck'))) {
       // Find cell which has data-truck-id attribute and get its ID
       const cellWithinTruck = Array.from(cellsWeHavePainted).find(cell => cell.hasAttribute('data-id'))
+      if (cellWithinTruck === undefined) {
+        makeNewBrick();
+        return
+      }
+
+
       const truckId = cellWithinTruck.getAttribute('data-id');
-      console.log(truckId)
       // Block truck with that id
+      blockedTruckIds[truckId] = true
+      if (truckId === '1') {
+        allCells.forEach((el, index) => {
+          if (index % boardSize < 11 && index % boardSize > 0) {
+            el.classList.add('blocked');
+          }
+        })
+      }
+      if (truckId === '2') {
+        allCells.forEach((el, index) => {
+          if (index % boardSize < 22 && index % boardSize > 11) {
+            el.classList.add('blocked');
+          }
+        })
+      }
+      if (truckId === '3') {
+        allCells.forEach((el, index) => {
+          if (index % boardSize > 22 && index % boardSize < 33) {
+            el.classList.add('blocked');
+          }
+        })
+      }
       // TODO
     }
-    
-    
+    cellsWeHavePainted.forEach(item => item.classList.add('blocked'));
 
-    currentBrickName = randomBrick();
-    y = -1;
-    x = randomBrickStart()
+
+    makeNewBrick();
     return;
   }
 
@@ -104,12 +179,12 @@ function paintingBricks() {
 
   // paint new cells
   cellsWeWantToPaint.forEach(cell => cell.classList.add('fruit'))
-  
+
 }
 
 // spadanie klocków
 setInterval(function () {
-  if(gameIsPaused){
+  if (gameIsPaused) {
     return;
   }
   y++;
--- gra/trucks.js
+++ gra/trucks.js
@@ -33,27 +33,20 @@ truckNodes.forEach((el, index) => {
 })
 
 truckNodes.forEach((el, index) => {
-        if (index % boardSize < 22 && index % boardSize > 12) {
+        if (index % boardSize < 22 && index % boardSize > 11) {
                 el.classList.add('truck-two');
+                el.setAttribute('data-id' , '2');
         }
 })
 
 truckNodes.forEach((el, index) => {
         if (index % boardSize > 22 && index % boardSize < 33) {
                 el.classList.add('truck-three');
+                el.setAttribute('data-id' , '3');
 
         }
 })
 
-function detectTruck() {
-        ourBricks = document.querySelectorAll('.fruit');
-        truckOne = document.querySelectorAll('.truck-one');
-
-
-        if (ourBricks.classList.contains('truck')) {
-                truckOne.forEach((el) => {
-                        el.classList.remove('truck');
-                        el.classList.add('wall')
-                })
-        }
+function blockTruck() {
+        
 }
