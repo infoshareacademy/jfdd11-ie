7b31e57cf63274d17899105353a9464e42dbe613
magda <magda17095@wp.pl>
2019-02-08 15:57:06 +0100

    stylowanie okna gry

--- gra/bricks.js
+++ gra/bricks.js
@@ -68,11 +68,6 @@ window.addEventListener("keydown", function (event) {
 });
 let currentBrickName = randomBrick();
 let currentBrickFrame = 0;
-addEventListener("keydown", function (event) {
-  if (event.code === "Space") {
-    currentBrickFrame = (currentBrickFrame + 1) % 4;
-  }
-});
 
 
 
