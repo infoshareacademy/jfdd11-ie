547f2d9cf71d8ff4c7d4e5287b48aa8aa7881310
Maciej Mazur <mr.mazurmaciej@gmail.com>
2019-02-11 16:49:44 +0100

    i commit new structure with two folders: landing page is the home, and game.

---
+++ home/js/script.js
@@ -0,0 +1,13 @@
+let consent = document.querySelector(".form-consent-box");
+let checkbox = document.querySelector(".form-consent-checkbox");
+
+consent.addEventListener("click", function(){
+    
+    if(checkbox.checked === false){
+        checkbox.checked = true;
+    }else{
+        checkbox.checked = false;
+    }
+});
+
+
