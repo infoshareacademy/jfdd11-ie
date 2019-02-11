8bff1d76e7481196afd24455ff595f667568058a
Maciej Mazur <mr.mazurmaciej@gmail.com>
2019-02-11 16:44:03 +0100

    i prepered space for refactoring code in right folders and files.

--- gra/collisions.js
+++
@@ -1,3 +0,0 @@
-// przyjąłem prowizorycznny ruch klocków żeby ustalić kolizje. dla demonstracji tworzę klocka na 1 komórce i przeszkodę na 5 komórce. prowizoryczny ruch odbywa się tylko w prawo i lewo (sterowanie strzałkami). warunek kolizji jest ustawiony na krawędź planszy (null) i na elementy z klasą "blocked". 
-
-// przykładowy klocek
